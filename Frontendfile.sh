b371060c65db8646a1807aff7446236f095192c0
kenan M <kenan@outofhours.io>
2025-03-21 14:39:27 +0000

    Update Frontendfile.sh

--- Frontendfile.sh
+++ Frontendfile.sh
@@ -33,63 +33,57 @@ check_exit_status "apt upgrade"
 # Install the AWS CLI tool using Snap for managing AWS resources
 snap install aws-cli --classic
 
-# Clone the GitHub repository
-#log "Cloning GitHub repository..."
-#sudo git clone https://github.com/kenanam/epa-project.git /root/wordpress-project 
-#check_exit_status "git clone"
+# (Git clone and permission changes are handled by GitHub Actions)
 
-# Change permissions of the cloned repository
-#log "Changing permissions of the cloned repository..."
-#sudo chmod -R 755 /root/wordpress-project
-#check_exit_status "chmod"
-
-# Run the setup script
 log "Running lemp-setup.sh script..."
 
 sudo apt update -y
 sudo apt upgrade -y
-sudo touch /root/testing.txt # this file will contain the output of our LEMP stack unit tests
+sudo touch /root/testing.txt  # LEMP stack unit tests output
 sudo apt -y install nginx
-sudo systemctl start nginx && sudo systemctl enable nginx # this starts and enables nginx on a server reboot. The 2nd command will only run if the first command is successful
+sudo systemctl start nginx && sudo systemctl enable nginx
 sudo systemctl status nginx > /root/testing.txt
 sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
 sudo php -v >> /root/testing.txt
-sudo systemctl stop apache2 # stops apache because we're aleady using nginx
-sudo systemctl disable apache2 # disables apache from starting on a server reboot
+sudo systemctl stop apache2    # Stop Apache since we're using Nginx
+sudo systemctl disable apache2   # Disable Apache from rebooting
 
-sudo mv /var/www/html/index.html /var/www/html/index.html.old # rename apache testing page
-# Instead of:
-# sudo mv /root/wordpress-project/configs/nginx.conf /etc/nginx/conf.d/nginx.conf
+sudo mv /var/www/html/index.html /var/www/html/index.html.old  # Rename Apache test page
 
-# Use:
+# Move the nginx.conf from the repository to Nginx's configuration directory
 sudo mv /home/ubuntu/epa-project/nginx.conf /etc/nginx/conf.d/nginx.conf
 
-
-# dns_record=$(curl -s icanhazip.com | sed 's/^/ec2-/; s/\./-/g; s/$/.compute-1.amazonaws.com/')
+# ===== Change Starts Here =====
+# Use a single domain variable: my_domain
 my_domain=REPLACE_DOMAIN
+# (Optional: if you do not need elastic_ip in your script, you may remove or ignore this line)
 elastic_ip=REPLACE_FRONTEND_IP
 
-# Update nginx configuration file
-sed -i "s/REPLACE_DOMAIN/$my_domain/g" /etc/nginx/conf.d/nginx.conf
-nginx -t && systemctl reload nginx # this will only reload nginx if the test is successful
+# Instead of performing a substitution here, rely on the workflow to replace placeholders.
+# Remove or comment out the following two lines if using centralized workflow substitutions:
+# sed -i "s/REPLACE_DOMAIN/$my_domain/g" /etc/nginx/conf.d/nginx.conf
+# nginx -t && systemctl reload nginx
+# ===== Change Ends Here =====
 
-# Update package list and install Certbot and Certbot Nginx plugin
 log "Installing Certbot and Certbot Nginx plugin..."
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install -y certbot python3-certbot-nginx
 
-# Define your email
+# ===== Change Starts Here =====
+# Use my_domain for Certbot instead of a separate DOMAIN variable.
 EMAIL=REPLACE_EMAIL
-DOMAIN=REPLACE_DOMAIN
-
-# Use Certbot to obtain and install the SSL certificate
+# Remove the separate DOMAIN variable declaration; use my_domain in its place.
+# DOMAIN=REPLACE_DOMAIN
+# Now, update Certbot to use $my_domain:
 log "Obtaining and installing SSL certificate..."
-sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL -d $DOMAIN || {
+sudo certbot --nginx --non-interactive --agree-tos --email $EMAIL -d $my_domain || {
     log "Certbot failed to obtain certificate"
     exit 1
 }
-# Nginx unit test that will reload Nginx to apply changes ONLY if the test is successful
+# ===== Change Ends Here =====
+
+# Nginx unit test to reload Nginx if the test is successful
 sudo nginx -t && systemctl reload nginx
 
 # Install WordPress
@@ -106,7 +100,7 @@ sudo chown -R www-data:www-data /var/www/html/
 sudo find /var/www/html/ -type d -exec chmod 0755 {} \;
 sudo find /var/www/html/ -type f -exec chmod 0644 {} \;
 
-# Update wp-config.php with the database credentials
+# Update wp-config.php with database credentials and backend IP
 sed -i "s/username_here/DB_USERNAME/g" /var/www/html/wp-config.php
 sed -i "s/password_here/DB_PASSWORD/g" /var/www/html/wp-config.php
 sed -i "s/database_name_here/DB_USERNAME/g" /var/www/html/wp-config.php
@@ -116,15 +110,13 @@ SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
 STRING='put your unique phrase here'
 printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/html/wp-config.php
 
-# Install the AWS CLI tool using Snap for managing AWS resources
+# Install the AWS CLI tool using Snap (if needed)
 snap install aws-cli --classic
 
-# This securely backsup and stores the wp-config.php credentials on S3 
+# Securely back up wp-config.php to S3
 aws s3 cp /var/www/html/wp-config.php s3://my-wp-deploy-bucket
 
-# Install chkrootkit vulnerability scanning tool.
+# Install chkrootkit vulnerability scanning tool and run it
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install chkrootkit -y
-
-# Run chrootkit scanning tool
 sudo chkrootkit > vulnerability_scan_output.txt
