e895e1669006d59d2cc3ee562f137cd92595823d
kenan M <kenan@outofhours.io>
2025-03-28 09:48:45 +0000

    Update Frontendfile.sh
    Adding - S3 command to securely to download my chatbot

--- Frontendfile.sh
+++ Frontendfile.sh
@@ -96,6 +96,9 @@ sudo unzip /var/www/latest.zip -d /var/www/
 sudo rm /var/www/latest.zip
 sudo mv /var/www/wordpress /var/www/html
 
+# This is the line to install my thg-chatbot
+aws s3 cp s3://my-wp-deploy-bucket/thg-chatbot /var/www/html/wp-content/plugins/thg-chatbot --recursive
+
 sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 sudo chmod 640 /var/www/html/wp-config.php 
 sudo chown -R www-data:www-data /var/www/html/
